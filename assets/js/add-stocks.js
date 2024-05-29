16e0cfcd7ded806fa6f52d1f9da6fd69bcec07b4
syncmi <syncmiusercare@gmail.com>
2024-05-29 14:09:51 +0530

    Stock In

--- assets/js/add-stocks.js
+++ assets/js/add-stocks.js
@@ -24,152 +24,176 @@ function capitalize(string) {
   });
 }
 
-//DB Schema
-//Collection-Items
-//  ->Document-Item-Name
-//      ->Item
-//      ->Rate
-//      ->Category
-
-var itemsHTML = ``;
-// Showing the items for adding them to the Billing Section
-const dbRef = database.ref();
-dbRef
-  .child("items")
-  .get()
-  .then((snapshot) => {
-    if (snapshot.exists()) {
-      snapshot.forEach((childSnapshot) => {
-        const key = childSnapshot.key;
-        const value = childSnapshot.val();
-        itemsHTML += `
-                          <tr id='${key}'>
-                            <td>${key}</td>
-                            <td>${value}</td>
-                            <td>
-                              <span class="text-primary"
-                                ><a href="update-item.html?item=${key}"><i class="bi bi-pencil-fill"></i
-                              ></a></span>
-                            </td>
-                            <td>
-                              <span class="text-danger"
-                                ><a data-bs-toggle="modal" data-bs-target="#addToStock" href="#" onclick="getItem('${key}','${value}')"><i class="bi bi-bag-plus-fill"></i></a>
-                              </span>
-                            </td>
-                          </tr>
-        `;
+function initialiseItems() {
+  // Check if items are already in local storage
+  const localItems = localStorage.getItem("items");
+  if (localItems) {
+    //console.log('Items already in local storage:', JSON.parse(localItems));
+    console.log("Local Storage - Items Available");
+  } else {
+    // Fetch items from Firebase Realtime Database
+    const itemsRef = database.ref("/items");
+    itemsRef
+      .once("value")
+      .then((snapshot) => {
+        const items = snapshot.val();
+        if (items) {
+          // Store items in local storage
+          localStorage.setItem("items", JSON.stringify(items));
+          console.log("Local Storage - Items Loaded");
+        } else {
+          console.log("No items found in database.");
+        }
+      })
+      .catch((error) => {
+        alert("Items Loading: " + error);
       });
-      document.getElementById("table-body-items").innerHTML = itemsHTML;
-      filterRows("");
-      document.getElementById("totalPages").textContent = countPages();
-    } else {
-      console.log("No data available");
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
-function getItem(itemName, itemCat) {
-  stockName = capitalize(itemName.trim().toLowerCase()).toString();
-  stockCat = itemCat;
-  console.log(stockName, stockCat);
-
-  document.querySelector(
-    "#addToStock .modal-title"
-  ).textContent = `Add to Stock - ${stockName}`;
+  }
 }
 
-const paidButton = document.getElementById("paid");
-var stockPaid = false;
+function loadItems() {
+  const localItems = JSON.parse(localStorage.getItem("items"));
 
-paidButton.addEventListener("change", function () {
-  if (this.checked) {
-    stockPaid = true;
-    console.log(stockPaid);
-  } else {
-    stockPaid = false;
+  // Reference to the table body element
+  const tableBody = document.getElementById("table-body-items");
 
-    console.log(stockPaid);
+  // Iterate over the items and append rows to the table
+  for (const [key, value] of Object.entries(localItems)) {
+    // Create a new row element
+    const row = document.createElement("tr");
+    row.id = key; // Set the id attribute to the key
+
+    // Create table data cells and set their content
+    const keyCell = document.createElement("td");
+    const keyCellLink = document.createElement("a");
+    keyCellLink.href = `javascript:setItem('${key}')`;
+    keyCellLink.textContent = key;
+    keyCell.appendChild(keyCellLink);
+
+    // Append table data cells to the row
+    row.appendChild(keyCell);
+
+    // Append the row to the table body
+    tableBody.appendChild(row);
   }
-});
+  filterRows("");
+}
+
+function setItem(itemName) {
+  stockName = capitalize(itemName.trim().toLowerCase()).toString();
+  document.getElementById("item-name").value = stockName;
+}
+
+function getTimestamp() {
+  //fulltime = new Date();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  const milliseconds = date.getMilliseconds().toString().padStart(3, "0");
+
+  // Determine if it's AM or PM
+  const period = hours >= 12 ? "PM" : "AM";
+
+  // Convert hours to 12-hour format
+  const formattedHours = (hours % 12 || 12).toString().padStart(2, "0"); // If hours is 0, convert to 12
+
+  // Construct the formatted date and time string
+  const formattedDate = `${day}-${month}-${year}`;
+  const formattedDateTime = `${day}-${month}-${year} ${formattedHours}:${minutes}:${seconds} ${period}`;
+  const formattedBillId = `${day}-${month}-${year}-${hours}-${minutes}-${seconds}-${milliseconds}`;
+
+  return [formattedDate, formattedBillId];
+}
+
 
 function addItem() {
-  // var stockName = stockName
-  // var stockCat = stockCat
-  var stockSeller = capitalize(
-    document.getElementById("stockSeller").value.trim().toLowerCase()
+  var stockName = capitalize(
+    document.getElementById("item-name").value.trim().toLowerCase()
   );
-  var stockQty = parseInt(document.getElementById("stockQty").value, 10);
-  var stockPrice = parseInt(document.getElementById("stockPrice").value, 10);
+  var stockSeller = capitalize(
+    document.getElementById("seller-name").value.trim().toLowerCase()
+  );
+  var stockQty = parseInt(document.getElementById("item-qty").value, 10);
+  var stockPrice = parseInt(document.getElementById("item-price").value, 10);
 
-  var stockPaidVal = stockPaid.toString();
-
-  function getTimestamp() {
-    //fulltime = new Date();
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const day = date.getDate().toString().padStart(2, "0");
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    const seconds = date.getSeconds().toString().padStart(2, "0");
-    const milliseconds = date.getMilliseconds().toString().padStart(3, "0");
-
-    // Determine if it's AM or PM
-    const period = hours >= 12 ? "PM" : "AM";
-
-    // Convert hours to 12-hour format
-    const formattedHours = (hours % 12 || 12).toString().padStart(2, "0"); // If hours is 0, convert to 12
-
-    // Construct the formatted date and time string
-    const formattedDate = `${day}-${month}-${year}`;
-    const formattedDateTime = `${day}-${month}-${year} ${formattedHours}:${minutes}:${seconds} ${period}`;
-    const formattedBillId = `${day}-${month}-${year}-${hours}-${minutes}-${seconds}-${milliseconds}`;
-
-    dateID = `${day}-${month}-${year}`;
-    fulltime = `${day}-${month}-${year}-${hours}-${minutes}-${seconds}-${milliseconds}`;
-    readableTime = date.toISOString();
-    return dateID;
-  }
-
-  const docRef = db.collection("stockin").doc(getTimestamp());
+  var stockPaidVal = document.getElementById("stock-paid").value == "true" ? true : false;
 
   const docData = {
-    [fulltime]: {
+    [getTimestamp()[1]]: {
       item: {
         name: stockName,
-        category: stockCat,
         qty: stockQty,
         price: stockPrice,
-        time: readableTime,
       },
       seller: stockSeller,
       paid: stockPaidVal,
     },
   };
 
-  docRef
+  db.collection("items")
+    .doc(stockName)
     .get()
-    .then(() => {
-      db.collection("stockin")
-        .doc(getTimestamp())
-        .set(docData, { merge: true })
-        .then(() => {
-          console.log("Document(Item) successfully written!");
-          document.getElementById("alert-msg").textContent =
-            stockName + " - Item Added!";
-          alert(stockname + " - Stock added");
-        })
-        .catch((error) => {
-          console.error("Error writing document: ", error);
-          document.getElementById("alert-msg").textContent =
-            "Error Occured, Try Again!";
-        });
+    .then((docSnapshot) => {
+      if (docSnapshot.exists) {
+        // Document exists, update the field
+        db.collection("items")
+          .doc(stockName)
+          .update({
+            stock: firebase.firestore.FieldValue.increment(stockQty),
+          })
+          .then(() => {
+            db.collection("stockin")
+              .doc(getTimestamp()[0])
+              .set(docData, { merge: true })
+              .then(() => {
+                console.log("Document(Item) successfully written!");
+                document.getElementById("alert-msg").textContent =
+                  stockName + " - Stock Added!";
+              })
+              .catch((error) => {
+                console.error("Error writing document: ", error);
+                document.getElementById("alert-msg").textContent = error;
+              });
+          })
+          .catch((error) => {
+            console.error("Error updating document: ", error);
+          });
+      } else {
+        alert("No item !");
+      }
     })
     .catch((error) => {
       console.error("Error getting document: ", error);
-      document.getElementById("alert-msg").textContent =
-        "Error Occured, Try Again!";
     });
 }
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    var uid = user.uid;
+    var displayName = user.displayName;
+    if (displayName != null) {
+      console.log("Name: " + displayName);
+    } else {
+      alert("Set your Name in the 'My Account' section");
+      window.location.href = "my-account.html";
+    }
+    var emailVerified = user.emailVerified;
+    if (emailVerified) {
+      initialiseItems();
+      loadItems();
+      const preloader = document.querySelector("#preloader");
+      preloader.remove();
+    } else {
+      alert("Verify your mail in the 'My account' section");
+      window.location.href = "my-account.html";
+    }
+  } else {
+    // User is signed out
+    // ...
+    window.location.href = "login.html";
+  }
+});
--- assets/js/stockin.js
+++ assets/js/stockin.js
@@ -20,182 +20,189 @@ var database = firebase.database();
 
 var fetchingDate;
 
+var formattedDate = getTimestamp();
+
 function getTimestamp() {
   const date = new Date();
   const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-  const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
-  var timestamp = `${day}-${month}-${year}`
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  const milliseconds = date.getMilliseconds().toString().padStart(3, "0");
+  var timestamp = `${day}-${month}-${year}`;
+  document.getElementById("stock-in-date").textContent = timestamp;
   fulltime = Date.now().toString();
   return timestamp;
 }
+function convertTo12HourFormat(hr, min, sec) {
+  let period = "AM";
 
-document.getElementById("stock-in-date").textContent = getTimestamp();
+  // Convert hours to 12-hour format and determine AM/PM
+  if (hr >= 12) {
+    period = "PM";
+    if (hr > 12) {
+      hr -= 12;
+    }
+  } else if (hr === 0) {
+    hr = 12; // Midnight case
+  }
 
+  // Ensure minutes and seconds are two digits
+  min = min.toString().padStart(2, "0");
+  sec = sec.toString().padStart(2, "0");
+
+  // Format the time
+  const time = `${hr}:${min}:${sec} ${period}`;
+  return time;
+}
 function fetchByDate(dateString) {
-
   const date = dateString;
   const year = date.slice(0, 4);
   const month = date.slice(5, 7);
   const day = date.slice(8, 10);
   fetchingDate = `${day}-${month}-${year}`;
 
-  document.getElementById("stocks").innerHTML = "";
-
   var docRef = db.collection("stockin").doc(fetchingDate);
   docRef.get().then((doc) => {
     if (doc.exists) {
-      itemKeys = Object.keys(doc.data());
-      itemNo = itemKeys.length;
-      stocks = doc.data();
-      console.log(itemKeys, itemNo);
+      var itemKeys = Object.keys(doc.data()).sort();
+      var itemNo = itemKeys.length;
+      var stocks = doc.data();
 
+      document.getElementById("stock-in-date-length").textContent = itemNo;
+      var stocksHTML = ``;
       for (i = 0; i < itemNo; i++) {
-
         var hr = itemKeys[i].slice(11, 13);
-        var min = itemKeys[i].slice(14, 16)
-        var sec = itemKeys[i].slice(17, 19)
-        var time = `${hr}:${min}:${sec}`;
-        console.log(time);
-
-        document.getElementById("stocks").innerHTML += `<div class="col-lg-3" id="${itemKeys[i]}">
+        var min = itemKeys[i].slice(14, 16);
+        var sec = itemKeys[i].slice(17, 19);
+        var time = convertTo12HourFormat(hr, min, sec);
+        stocksHTML += `<div class="col-lg-4" id="${itemKeys[i]}">
               <div class="card stockItem">
                 <div class="card-body">
                   <h5 class="card-title">${fetchingDate} ${time}</h5>
-                  <h5 class="card-subtitle mb-2 mt-2 text-muted">Paid: ${stocks[itemKeys[i]].paid} </h5>
+                  <h5 class="card-subtitle mb-2 mt-2 text-muted">Paid: ${
+                    stocks[itemKeys[i]].paid
+                  } </h5>
                   <p class="card-text">
                     <b> <span>${stocks[itemKeys[i]].item.name}</span> </b> <br>
                     <span>Quantity: ${stocks[itemKeys[i]].item.qty} </span> <br>
                     <span>Price: ${stocks[itemKeys[i]].item.price}</span> <br>
-                    <span>Rate: ${Number((stocks[itemKeys[i]].item.price/stocks[itemKeys[i]].item.qty).toFixed(2))} </span> <br>
+                    <span>Rate: ${Number(
+                      (
+                        stocks[itemKeys[i]].item.price /
+                        stocks[itemKeys[i]].item.qty
+                      ).toFixed(2)
+                    )} </span> <br>
                     <span>Seller: ${stocks[itemKeys[i]].seller}</span>
                   </p>
                   <p class="card-text">
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#editStock" class="btn btn-primary" onclick="getItem('${itemKeys[i]}')">Edit</a>
-                    <a href="javascript:deleteItem('${itemKeys[i]}', '${stocks[itemKeys[i]].item.name}')" class="btn btn-primary">Delete</a>
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#editStock" class="btn btn-primary" onclick="getItem('${
+                      itemKeys[i]
+                    }')">Edit</a>
+                    <a href="javascript:deleteItem('${itemKeys[i]}', '${
+          stocks[itemKeys[i]].item.name
+        }')" class="btn btn-primary">Delete</a>
                   </p>
                 </div>
               </div>
-            </div>`
+            </div>`;
       }
-
+      document.getElementById("stocks").innerHTML = stocksHTML;
+    } else {
+      alert("No Stock in found on " + fetchingDate + " !");
     }
-  }
-  );
-
+  });
 }
 
-// function filterStocks(query) {
-//   var stocks = document.getElementsByClassName("stockItem");
-//   var allstocks = stocks;
-//   var found = false;
+async function loadStockIn() {
+  docRef = db.collection("stockin").doc(formattedDate);
+  await docRef.get().then((doc) => {
+    if (doc.exists) {
+      var itemKeys = Object.keys(doc.data()).sort();
+      var itemNo = itemKeys.length;
+      var stocks = doc.data();
 
-//   for (var i = 0; i < stocks.length; i++) {
-//     if (stocks[i].innerHTML.toString().toLowerCase().includes(query.toLowerCase())) {
-//       found = true;
-//       break;
-//     }
-//   }
+      document.getElementById("stock-in-date-length").textContent = itemNo;
 
-//   for (var i = 0; i < stocks.length; i++) {
-//     if (found && !stocks[i].innerHTML.toString().toLowerCase().includes(query.toLowerCase())) {
-//       stocks[i].style.display = "none";
-//     } else {
-//       stocks[i].style.display = "block";  
-//     }
-//   }
-// }
+      var stocksHTML = ``;
+      for (i = 0; i < itemNo; i++) {
+        var hr = itemKeys[i].slice(11, 13);
+        var min = itemKeys[i].slice(14, 16);
+        var sec = itemKeys[i].slice(17, 19);
+        var time = convertTo12HourFormat(hr, min, sec);
 
-
-// var docRef = db.collection("stockin").doc(getTimestamp());
-// docRef.get().then((doc) => {
-//   if (doc.exists) {
-//     itemKeys = Object.keys(doc.data())
-//     itemNo = itemKeys.length;
-//     console.log(itemKeys, itemNo);
-//     // console.log("Document data:", doc.data());
-//     for (i = 0; i < itemNo; i++) {
-//       console.log(doc.data()[itemKeys[i]]);
-//     }
-
-//   }
-// }
-// );
-
-docRef = db.collection("stockin").doc(getTimestamp());
-docRef.get().then((doc) => {
-  if (doc.exists) {
-    itemKeys = Object.keys(doc.data())
-    itemNo = itemKeys.length;
-    stocks = doc.data();
-    console.log(itemKeys, itemNo);
-
-    for (i = 0; i < itemNo; i++) {
-      var hr = itemKeys[i].slice(11, 13);
-      var min = itemKeys[i].slice(14, 16)
-      var sec = itemKeys[i].slice(17, 19)
-      var time = `${hr}:${min}:${sec}`;
-      console.log(stocks[itemKeys[i]]);
-
-      document.getElementById("stocks").innerHTML += `
-        <div class="col-lg-3" id="${itemKeys[i]}">
+        stocksHTML += `
+        <div class="col-lg-4" id="${itemKeys[i]}">
               <div class="card stockItem">
                 <div class="card-body">
                   <h5 class="card-title">${getTimestamp()} ${time}</h5>
-                  <h5 class="card-subtitle mb-2 mt-2 text-muted">Paid: ${stocks[itemKeys[i]].paid} </h5>
+                  <h5 class="card-subtitle mb-2 mt-2 text-muted">Paid: ${
+                    stocks[itemKeys[i]].paid
+                  } </h5>
                   <p class="card-text">
                     <b> <span>${stocks[itemKeys[i]].item.name}</span> </b> <br>
                     <span>Quantity: ${stocks[itemKeys[i]].item.qty} </span> <br>
                     <span>Price: ${stocks[itemKeys[i]].item.price}</span> <br>
-                    <span>Rate: ${Number((stocks[itemKeys[i]].item.price/stocks[itemKeys[i]].item.qty).toFixed(2))} </span> <br>
+                    <span>Rate: ${Number(
+                      (
+                        stocks[itemKeys[i]].item.price /
+                        stocks[itemKeys[i]].item.qty
+                      ).toFixed(2)
+                    )} </span> <br>
                     <span>Seller: ${stocks[itemKeys[i]].seller}</span>
                   </p>
                   <p class="card-text">
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#editStock" class="btn btn-primary" onclick="getItem('${itemKeys[i]}')">Edit</a>
-                    <a href="javascript:deleteItem('${itemKeys[i]}', '${stocks[itemKeys[i]].item.name}')" class="btn btn-primary">Delete</a>
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#editStock" class="btn btn-primary" onclick="getItem('${
+                      itemKeys[i]
+                    }')">Edit</a>
+                    <a href="javascript:deleteItem('${itemKeys[i]}', '${
+          stocks[itemKeys[i]].item.name
+        }')" class="btn btn-primary">Delete</a>
                   </p>
                 </div>
               </div>
             </div>
-            `
+            `;
+      }
+      document.getElementById("stocks").innerHTML = stocksHTML;
+    } else {
+      alert("No Stock in Found on " + formattedDate + " !");
     }
-
-  }
+  });
 }
-);
 
 function deleteItem(key, stockName) {
   console.log(key, stockName);
-  var result = confirm("Are you sure you want to delete " + stockName + " ?");
+  var result = confirm("Are you sure you want to delete " + key + " ?");
   if (result) {
-    db.collection("stockin").doc(key.slice(0, 10)).update({
-      [key]: firebase.firestore.FieldValue.delete()
-    }
-    ).then(
-      alert(`${stockName} deleted successfully`)
-    ).then(
-      document.getElementById(`${key}`).remove()
-    )
+    db.collection("stockin")
+      .doc(key.slice(0, 10))
+      .update({
+        [key]: firebase.firestore.FieldValue.delete(),
+      })
+      .then(alert(`${key} deleted successfully`))
+      .then(document.getElementById(`${key}`).remove())
       .catch(function (error) {
         console.error("Error removing document: ", error);
       });
   }
 }
+
 var itemData;
 var itemKey;
 
 function getItem(key) {
   itemKey = key;
   console.log(key);
-  var docRef = db.collection('stockin').doc(key.slice(0, 10))
+  var docRef = db.collection("stockin").doc(key.slice(0, 10));
 
   docRef.get().then((doc) => {
     var itemDetails = doc.data()[key];
-    document.querySelector("#editStock .modal-title").innerHTML = `Edit Stock - ${itemDetails.item.name}`;
+    document.querySelector(
+      "#editStock .modal-title"
+    ).innerHTML = `Edit Stock - ${itemDetails.item.name}`;
     itemData = {
       name: itemDetails.item.name,
       cat: itemDetails.item.category,
@@ -203,38 +210,138 @@ function getItem(key) {
       price: itemDetails.item.price,
       time: itemDetails.item.time,
       seller: itemDetails.seller,
-      paid: itemDetails.paid
-    }
-    document.querySelector("#editStock .iteminputs #stockName").value = itemData.name;
-    document.querySelector("#editStock .iteminputs #stockSeller").value = itemData.seller;
-    document.querySelector("#editStock .iteminputs #stockQty").value = itemData.qty;
-    document.querySelector("#editStock .iteminputs #stockPrice").value = itemData.price;
-    document.querySelector("#editStock .iteminputs #paid").checked = itemData.paid;
-    console.log(document.querySelector("#editStock .iteminputs #paid").checked)
-  }
-  )
-
+      paid: itemDetails.paid,
+    };
+    document.querySelector("#editStock .iteminputs #stockName").value =
+      itemData.name;
+    document.querySelector("#editStock .iteminputs #stockSeller").value =
+      itemData.seller;
+    document.querySelector("#editStock .iteminputs #stockQty").value =
+      itemData.qty;
+    document.querySelector("#editStock .iteminputs #stockPrice").value =
+      itemData.price;
+    document.querySelector("#editStock .iteminputs #paid").checked =
+      itemData.paid;
+    console.log(document.querySelector("#editStock .iteminputs #paid").checked);
+  });
 }
 
 function editItem() {
-
-  db.collection("stockin").doc(itemKey.slice(0, 10)).update({
-    [itemKey]: {
-      item: {
-        name: document.querySelector("#editStock .iteminputs #stockName").value,
-        qty: document.querySelector("#editStock .iteminputs #stockQty").value,
-        price: document.querySelector("#editStock .iteminputs #stockPrice").value,
-
+  db.collection("stockin")
+    .doc(itemKey.slice(0, 10))
+    .update({
+      [itemKey]: {
+        item: {
+          name: document.querySelector("#editStock .iteminputs #stockName")
+            .value,
+          qty: document.querySelector("#editStock .iteminputs #stockQty").value,
+          price: document.querySelector("#editStock .iteminputs #stockPrice")
+            .value,
+        },
+        paid: document.querySelector("#editStock .iteminputs #paid").checked,
+        seller: document.querySelector("#editStock .iteminputs #stockSeller")
+          .value,
       },
-      paid: document.querySelector("#editStock .iteminputs #paid").checked,
-      seller: document.querySelector("#editStock .iteminputs #stockSeller").value
+    })
+    .then(() => {
+      location.reload();
+    })
+    .catch((error) => {
+      console.log("Error editing the item: ", error);
+    });
+}
+
+firebase.auth().onAuthStateChanged(async (user) => {
+  if (user) {
+    var uid = user.uid;
+    var displayName = user.displayName;
+    if (displayName != null) {
+      console.log("Name: " + displayName);
+    } else {
+      alert("Set your Name in the 'My Account' section");
+      window.location.href = "my-account.html";
+    }
+    var emailVerified = user.emailVerified;
+    if (emailVerified) {
+      document.getElementById("userName").textContent = displayName;
+      await loadStockIn();
+      const preloader = document.querySelector("#preloader");
+      preloader.remove();
+      await deleteOldDocuments();
+    } else {
+      alert("Verify your mail in the 'My account' section");
+      window.location.href = "my-account.html";
+    }
+  } else {
+    // User is signed out
+    // ...
+    window.location.href = "login.html";
+  }
+});
+
+function signOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      alert("Signed Out Successfully!");
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function getFormattedDate(date) {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are 0-based
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
+}
+
+// Calculate the date 31 days ago
+function getCutoffDate() {
+  const today = new Date();
+  const cutoffDate = new Date(today);
+  cutoffDate.setDate(today.getDate() - 31);
+  return cutoffDate;
+}
+
+// Delete old documents
+async function deleteOldDocuments() {
+  const cutoffDate = getCutoffDate();
+  const todayFormatted = getFormattedDate(new Date());
+
+  // Get localStorage data
+  const stockInDeletion =
+    JSON.parse(localStorage.getItem("stockInDeletion")) || {};
+
+  // Check if the deletion for today has already been done
+  if (stockInDeletion.date === todayFormatted && stockInDeletion.deleted) {
+    console.log("Deletion already performed today.");
+    return;
+  }
+
+  // Delete documents older than the cutoff date
+  let deletionOccurred = false;
+  for (let i = 0; i < 31; i++) {
+    const dateToDelete = new Date(cutoffDate);
+    dateToDelete.setDate(cutoffDate.getDate() - i);
+    const dateToDeleteFormatted = getFormattedDate(dateToDelete);
+
+    try {
+      await db.collection("stockin").doc(dateToDeleteFormatted).delete();
+      console.log(`Deleted document with ID: ${dateToDeleteFormatted}`);
+      deletionOccurred = true;
+    } catch (error) {
+      console.error(
+        `Error deleting document (ID: ${dateToDeleteFormatted}): `,
+        error
+      );
     }
   }
-  ).then(() => {
-    location.reload()
-  }
-  ).catch((error) => {
-    console.log("Error editing the item: ", error);
-  })
 
+  // Update localStorage
+  stockInDeletion.date = todayFormatted;
+  stockInDeletion.deleted = deletionOccurred;
+  localStorage.setItem("stockInDeletion", JSON.stringify(stockInDeletion));
 }
